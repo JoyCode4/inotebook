9cdd5589997d85d6e123c4eb91015c04be4d47ba
JoyCode4 <joywadhonkar4@gmail.com>
2023-06-05 15:07:59 +0530

    created the POSTAPI checking of the auth proper and also done this to the changes in index.js, auth.js, User.js,db.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const {body, validationResult } = require("express-validator");
 
 
-// Create a User using : POST "/api/auth/". Doen't require Auth
-router.post("/",(req,res)=>{
+// Create a User using : POST "/api/auth/". No Login Required ,Doesn't require Auth
+router.post("/createuser",[
+    body("email","Enter a valid email").isEmail(),
+    body("name","Enter a valid name").isLength({min:3}),
+    body("password","Password must be atleast 5 characters").isLength({min:5}),
+],async (req,res)=>{
     // console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+    // If there are errors, return bad request and the errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    try{
+
+        // Check whether the user with this email exists already
+        let user = await User.findOne({email:req.body.email});
+        if(user){
+            return res.status(400).json({error:" Sorry,a user with this email already exists..."})
+        }
+        user = await User.create({
+            email:req.body.email,
+            password:req.body.password,
+            name:req.body.name
+        })
+        res.status(200).json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router;
