72ee78d1734c539948c8af4da73af78aed8480a8
JoyCode4 <joywadhonkar4@gmail.com>
2023-07-15 18:49:51 +0530

    jwttoken package is installed and the token is created and send as response

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const router = express.Router();
 const {body, validationResult } = require("express-validator");
 const bcrypt=require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "jayeshwadhonk@r";
 
 
 // Create a User using : POST "/api/auth/createuser". No Login Required ,Doesn't require Auth
@@ -31,7 +33,16 @@ router.post("/createuser",[
             password:secPass,
             name:req.body.name
         })
-        res.status(200).json(user);
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data,JWT_SECRET);
+        console.log(authtoken);
+
+        res.status(200).json({authtoken});
     }catch(err){
         console.error(err.message);
         res.status(500).send("Some error occured");
