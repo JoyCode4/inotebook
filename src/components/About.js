788f63534f75d0b9bda42c8d79779e2e901e4011
JoyCode4 <joywadhonkar4@gmail.com>
2023-07-25 15:01:48 +0530

    clean up the some of the useContext that is not needed

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,10 @@
-import React,{useContext, useEffect} from 'react';
-import NoteContext from '../context/notes/noteContext';
+import React from 'react';
+
 
 const About = () => {
-  const a=useContext(NoteContext);
-  useEffect(()=>{
-    a.update();
-     // eslint-disable-next-line
-  },[])
   return (
     <div>
-       <h1>This is my About page of React App author is {a.state.name} and his roll No is {a.state.roll}</h1>
+       <h1>This is my About page of React App </h1>
     </div>
   )
 }
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -1,23 +1,9 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
 
 
 const NoteState = (props) =>{
-    const [state,setState]=useState({
-        "name":"Jayesh",
-        "roll":10
-    })
-
-    const update=()=>{
-        setTimeout(()=>{
-            setState({
-                "name":"Parth",
-                "roll":9
-            })
-        },1000)
-    }
     return(
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
