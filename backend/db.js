f1ae1cd2e6616ba34f809b449aa8049eadccc558
JoyCode4 <joywadhonkar4@gmail.com>
2023-08-03 01:23:58 +0530

    coverted the env to string

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const mongoURI = process.env.MONGO_URL;
+const mongoURI = process.env.MONGO_URL.toString();
 
 const connectToMongo = async () =>{
     await mongoose.connect(mongoURI);
