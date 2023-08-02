401d63b55d0aee6e0518c3078b42666d2f10bc3d
JoyCode4 <joywadhonkar4@gmail.com>
2023-08-02 23:45:28 +0530

    mongo env is updated

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const mongoURI = "mongodb+srv://joy:joy00004@cluster.ywai3il.mongodb.net/test?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGO_URL;
 
 const connectToMongo = async () =>{
     await mongoose.connect(mongoURI);
